ad17ac262f256a6f13b68189b78c80226bae09d3
Valentine Samoylov <valentine.samoylov@gmail.com>
2023-01-22 22:22:43 +0200

    The active and focus states for button/a/inputs have been fixed.

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,15 +1,15 @@
 // Styles
-const styles = {
+export const styles = {
   layout: 'bg-white text-gray-500 dark:bg-slate-800 dark:text-gray-400',
   button: {
-    common: 'p-2 rounded-md border-none hover:transition-colors',
+    common: 'p-2 rounded-md border-none hover:transition-colors active:opacity-80',
     filled:
       'bg-gradient-to-br from-slate-600 to-slate-700 text-white shadow md:hover:shadow-sm md:hover:from-slate-500 md:hover:to-slate-600',
     icon: 'md:hover:bg-slate-900/10 dark:md:hover:bg-white/10',
   },
   tabBtn: {
     common:
-      'flex items-center gap-2 -mb-[3px] p-3 rounded-t-lg border-0 border-b-[3px] border-transparent text-2xl font-bold hover:transition-colors',
+      'flex items-center gap-2 -mb-[3px] p-3 rounded-t-lg border-0 border-b-[3px] border-transparent text-2xl font-bold hover:transition-colors active:opacity-80',
     active: 'border-slate-700 text-slate-700 dark:border-white dark:text-white',
     inactive: 'text-gray-500 md:hover:border-gray-400 md:hover:text-gray-400',
   },
@@ -19,5 +19,3 @@ const styles = {
     sm: 'text-xs font-semibold text-gray-500 uppercase tracking-wider dark:text-gray-400',
   },
 }
-
-export { styles }
