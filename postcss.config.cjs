7dbfb46451386d089f2a9acef69e51d6dac3f38b
Valentine Samoylov <valentine.samoylov@gmail.com>
2023-05-23 19:57:58 +0300

    Added postcss-preset-env plugin.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,10 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-mixins': {},
     'tailwindcss/nesting': 'postcss-nesting',
     tailwindcss: {},
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false },
+    },
   },
 }
